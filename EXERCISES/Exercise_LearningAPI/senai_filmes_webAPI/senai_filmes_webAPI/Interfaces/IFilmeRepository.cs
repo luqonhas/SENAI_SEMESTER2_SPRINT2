c41d9154ccbd962d46b3ef2c7e6239ece8a28fff
Lucas Apolinário <you@example.comlucas.apolinario98@gmail.com>
2021-03-19 19:00:45 -0300

    POST funcionando no Filmes também

--- EXERCISES/Exercise_LearningAPI/senai_filmes_webAPI/senai_filmes_webAPI/Interfaces/IFilmeRepository.cs
+++ EXERCISES/Exercise_LearningAPI/senai_filmes_webAPI/senai_filmes_webAPI/Interfaces/IFilmeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using senai_filmes_webAPI.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace senai_filmes_webAPI.Interfaces
 {
     interface IFilmeRepository
     {
+        List<FilmeDomain> ListarTodos();
+        FilmeDomain BuscarPorId(int id);
+        void Cadastrar(FilmeDomain novoFilme);
+        void AtualizarIdCorpo(FilmeDomain filme);
+        void AtualizarIdUrl(int id, FilmeDomain filme);
+        void Deletar(int id);
     }
 }
---
+++ EXERCISES/Exercise_LearningAPI/senai_filmes_webAPI/senai_filmes_webAPI/Repositories/FilmeRepository.cs
@@ -0,0 +1,83 @@
+﻿using senai_filmes_webAPI.Domains;
+using senai_filmes_webAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webAPI.Repositories
+{
+    public class FilmeRepository : IFilmeRepository
+    {
+        private string stringConexao = "Data Source=DESKTOP-HMTUR0P; initial catalog=Filmes; user Id=SA; pwd=Soufoda2";
+
+        public void AtualizarIdCorpo(FilmeDomain filme)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AtualizarIdUrl(int id, FilmeDomain filme)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FilmeDomain BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(FilmeDomain novoFilme)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(stringConexao))
+            {
+                string queryDelete = "DELETE Filmes WHERE Filmes.idFilmes ="+id;
+
+                using (SqlCommand command = new SqlCommand(queryDelete, connection))
+                {
+                    connection.Open();
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<FilmeDomain> ListarTodos()
+        {
+            List<FilmeDomain> listaFilmes = new List<FilmeDomain>();
+
+            using (SqlConnection connection = new SqlConnection(stringConexao))
+            {
+                string querySelectAll = "SELECT idFilmes, idGenero, Titulo FROM Filmes";
+
+                connection.Open();
+
+                SqlDataReader reader;
+
+                using (SqlCommand command = new SqlCommand(querySelectAll, connection))
+                {
+                    reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        FilmeDomain filme = new FilmeDomain()
+                        {
+                            idFilme = Convert.ToInt32(reader[0]),
+                            idGenero = Convert.ToInt32(reader[1]),
+                            titulo = reader[2].ToString()
+                        };
+
+                        listaFilmes.Add(filme);
+                    }
+
+                    return listaFilmes;
+                }
+            }
+        }
+    }
+}
